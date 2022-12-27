ab48eee78f8301795c03f437a321fc0631e896eb
asmaaEbeed <asmaaEbeed@users.noreply.github.com>
2022-12-27 15:07:54 +0200

    edit notification

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -8,7 +8,7 @@ const Notifications = (props) => {
     //   }`}
     
     return ReactDom.createPortal ((
-        <div className={`flex justify-between items-center text-gray-200 bg-gray-700 p-5 fixed shadow shadow-zinc-600 h-16 right-0 top-11 rounded-tl-lg rounded-bl-lg w-96 transition-all md:py-8 md:px-7 py-5 px-5 ${
+        <div className={`items-center text-gray-200 bg-gray-700 p-5 fixed shadow shadow-zinc-600 h-16 right-0 md:top-11 top-8 rounded-tl-lg rounded-bl-lg md:w-96 w-60 md:text-base text-sm transition-all md:py-8 md:px-7 py-2 px-2 ${
             status === "success" ? "bg-green-400 text-gray-900" : ""
           }`}>
           <h2>{title}</h2>
